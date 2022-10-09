496dca77b46455a0d0382b30dc11bf12e8552119
Gama <lic.camey@gmail.com>
2022-10-09 20:04:55 -0300

    Main

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+
+const Footer = () => {
+  return (
+    <div>
+    <p>I'm the footer</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Main/index.js
@@ -0,0 +1,15 @@
+import Footer from "../Footer";
+import Header from "../Header";
+import Home from "../Home";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
