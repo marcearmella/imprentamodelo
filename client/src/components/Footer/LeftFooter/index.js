5a45a17268e6fd6a5b91842f589df78187d07e6f
Gama <lic.camey@gmail.com>
2022-10-11 11:40:00 -0300

    Footer

---
+++ client/src/components/Footer/LeftFooter/index.js
@@ -0,0 +1,23 @@
+import "./index.css";
+
+const LeftFooter = () => {
+  return (
+    <div className="container">
+      <h1 className="title">ARTES GRÁFICAS MODELO</h1>
+      <div className="logo-container">
+        <img src="" alt="logoIM" />
+      </div>
+      <div className="text-zone">
+        <p>
+          Artes Gráficas Modelo es una imprenta familiar. Contamos con 30 años
+          en el rubro offset y entre otros servicios ofrecemos impresiones en
+          digital para pequeños a medianos proyectos, en pequeño y gran formato,
+          sobre papel, lona y vinilo entre otros, para lograr un abanico de
+          soluciones para su proyecto.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LeftFooter;
